665220af930697cd4fb2173e65742cb45fa2a1b6
kissa1001 <kissa101295@gmail.com>
2016-04-05 15:56:55 -0400

    added angular

--- io.js
+++ io.js
@@ -1,7 +1,19 @@
 var io = require('socket.io')();
+var users = {};
 
 io.on('connection', function(socket){
   console.log('New connection detected.');
+  socket.on('join', function(name){
+      socket.name = name;
+      console.log(name + ' join');
+      users[socket.name] = socket;
+      io.sockets.emit('usernames', Object.keys(users));
+  });
+  socket.on('disconnect', function(){
+  	console.log(socket.name + ' disconnected');
+    delete users[socket.nickname];
+  	io.sockets.emit('usernames', Object.keys(users));
+  });
 });
 
 module.exports = io;
